ed28106cbdeda39d5b7e74ccfa9580c1ff7b71c8
Sarthak Chatterjee <sarthatc@gmail.com>
2023-11-22 15:11:06 +0530

    Adding Comments

--- frontend/src/TextEditor.js
+++ frontend/src/TextEditor.js
@@ -6,85 +6,113 @@ import { io } from "socket.io-client";
 import { useParams } from "react-router-dom";
 
 const TextEditor = () => {
+
+  // Extract document ID from the URL
   const { id: docId } = useParams();
+
+  // State variables for managing the socket and Quill instances
   const [socket, setSocket] = useState();
   const [quill, setQuill] = useState();
 
-  const SAVE_INTERVAL = 1500;
+  const SAVE_INTERVAL = 1500;  // Save interval for periodically saving the document
 
+  // Effect to initialize the socket connection
   useEffect(() => {
+    // Connect to the Socket.io server
     const socketServer = io("http://localhost:3001");
     setSocket(socketServer);
 
+    // Cleanup function to disconnect the socket when the component unmounts
     return () => {
       socketServer.disconnect();
     };
   }, []);
 
+  // Effect to load the document from the server when the socket and Quill instances are ready
   useEffect(() => {
     if (socket == null || quill == null) return;
 
+    // Load the document content from the server
     socket.once("load-document", (document) => {
       quill.setContents(document);
       quill.enable();
     });
 
+    // Request the document content from the server
     socket.emit("get-document", docId);
   }, [socket, quill, docId]);
 
+
+  // Effect to periodically save the document to the server
   useEffect(() => {
     if (socket == null || quill == null) return;
 
+    // Setup interval for saving the document
     const interval = setInterval(() => {
       socket.emit("save-document", quill.getContents());
     }, SAVE_INTERVAL);
 
+    // Cleanup function to clear the interval when the component unmounts
     return () => {
       clearInterval(interval);
     };
   }, [socket, quill]);
 
+  // Effect to handle incoming changes from other users and update the Quill editor
   useEffect(() => {
     if (socket == null || quill == null) return;
 
+    // Event handler for receiving changes from other users
     const handler = (delta) => {
       quill.updateContents(delta);
     };
+
+    // Set up a socket listener for "receive-changes" and update the Quill contents 
     socket.on("receive-changes", handler);
 
+    // Cleanup function to clean the socket listener when the component unmounts
     return () => {
       socket.off("receive-changes", handler);
     };
   }, [socket, quill]);
 
+  // Effect to handle local changes and send them to the server
   useEffect(() => {
     if (socket == null || quill == null) return;
 
+    // Event handler for local text changes
     const handler = (delta, oldDelta, source) => {
-      if (source !== "user") return;
+      if (source !== "user") return; // Ignore non-user changes
       socket.emit("send-changes", delta);
     };
+
+    // Register the event handler with the Quill editor
     quill.on("text-change", handler);
 
+    // Cleanup function to unregister the event handler when the component unmounts
     return () => {
       quill.off("text-change", handler);
     };
   }, [socket, quill]);
 
+
+  // Callback function to set up the Quill editor when the wrapper element is available
   const wrapperRef = useCallback((wrapper) => {
     if (wrapper == null) return;
 
+    // Clear the wrapper and create a new Quill editor
     wrapper.innerHTML = "";
     const editor = document.createElement("div");
     wrapper.append(editor);
     const quillServer = new Quill(editor, {
-      theme: "snow",
-      // modules: { toolbar: TOOLBAR_OPTIONS },
+      theme: "snow"
     });
-    quillServer.disable();
+    quillServer.disable(); // Disable editing until the document is loaded
     quillServer.setText("Loading the document...");
-    setQuill(quillServer);
+    setQuill(quillServer); //Set the Quill instance in the state
   }, []);
+
+  // Render the Quill editor within a container
   return <div className="container" ref={wrapperRef}></div>;
 };
 
--- server.js
+++ server.js
@@ -5,37 +5,43 @@ const Text = require("./backend/textModel");
 
 const mongoose = require("mongoose");
 
+// Connect to MongoDB
 mongoose.connect("mongodb://localhost:27017/AutosaveDoc").then((data) => {
   console.log(`Mongodb connected with server: ${data.connection.host}`);
 });
 
-app.use(express.json());
-
+// Setup Socket.io with CORS configuration
 const io = require("socket.io")(3001, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"],
-    },
-  })
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 const defaultValue = "";
 
-io.on("connection", socket => {
-    socket.on("get-document", async docId => {
-      const document = await findOrCreateDoc(docId)
-      socket.join(docId)
-      socket.emit("load-document", document.textData)
-  
-      socket.on("send-changes", delta => {
-        socket.broadcast.to(docId).emit("receive-changes", delta)
-      })
-  
-      socket.on("save-document", async textData => {
-        await Text.findByIdAndUpdate(docId, { textData })
-      })
-    })
-  })
+// Socket.io connection event
+io.on("connection", (socket) => {
+  // Event when a document is requested
+  socket.on("get-document", async (docId) => {
+    // Find or create document based on the document id
+    const document = await findOrCreateDoc(docId);
+    socket.join(docId); // Join the socket room corresponding to the document
+    socket.emit("load-document", document.textData); // Emit the document data to the client
 
+    // Event when changes are received from a client
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(docId).emit("receive-changes", delta); // Broadcast changes to other clients in the same room
+    });
+
+    // Event when a client wants to save the document
+    socket.on("save-document", async (textData) => {
+      await Text.findByIdAndUpdate(docId, { textData }); // Update the document in the database
+    });
+  });
+});
+
+// Function to find or create a document in the database
 async function findOrCreateDoc(id) {
   if (id == null) return;
 
@@ -44,6 +50,7 @@ async function findOrCreateDoc(id) {
   return await Text.create({ _id: id, textData: defaultValue });
 }
 
+// Start the server on port 8000
 const PORT = 8000;
 
 const server = app.listen(PORT, () => {
