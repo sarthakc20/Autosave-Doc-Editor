7638c8c19be25035660c176a06dd6e670af3fd4f
Sarthak Chatterjee <sarthatc@gmail.com>
2023-11-22 15:15:26 +0530

    Adding Comments to Schema

--- backend/textModel.js
+++ backend/textModel.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
+// Define a Mongoose schema for the "Text" collection
 const textSchema = new mongoose.Schema({
     _id: String,
     textData: Object,
 });
 
+// Create a Mongoose model for the "Text" collection using the defined schema
 module.exports = mongoose.model("Text", textSchema);
